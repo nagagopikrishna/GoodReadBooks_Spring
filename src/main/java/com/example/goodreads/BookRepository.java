6843ac474a3ba2e3b5fd30dd39dfff5a72002456
nagagopikrishna <nagagopikrishna4@gmail.com>
2025-06-10 01:37:46 +0000

    Rest API Completed

---
+++ src/main/java/com/example/goodreads/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.goodreads;
+
+
+import java.util.ArrayList;
+import com.example.goodreads.Book;
+
+public interface BookRepository{
+    ArrayList<Book> getBooks(); 
+    Book getBookById(int bookId);
+}
